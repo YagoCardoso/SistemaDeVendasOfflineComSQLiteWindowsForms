c77ee330908517a44de9b8879c5db7a443ea4edf
Marcus Vinicius Nobre Carraschi <marcus.carraschi@linx.com.br>
2024-05-26 19:26:52 -0300

    🎉 : Begin a project Domain with Customer refactoring - Add some business rules

---
+++ Pdv.Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Pdv.Domain.Entidades;
+
+namespace Pdv.Domain.Repositories
+{
+    public interface IClienteRepository
+    {
+        void InserirCliente(Cliente cliente);
+        void AlterarCliente(Cliente cliente);
+        Cliente ObterCliente(string cpf);        
+        void DeleteCliente(string cpf);
+    }
+}
