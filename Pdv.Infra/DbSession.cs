94298ead8f4084e5e089c018fd30d98e95d4f8af
Marcus Vinicius Nobre Carraschi <marcus.carraschi@linx.com.br>
2024-05-26 19:25:36 -0300

    🧱 - Add a layer of databse - Split some things of code.

---
+++ Pdv.Infra/DbSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Pdv.Infra
+{
+    public sealed class DbSession : IDisposable
+    {
+        private Guid _id;
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; set; }
+
+        public DbSession()
+        {
+            _id = Guid.NewGuid();
+            Connection = new SqlConnection(Settings.ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+    }
+}
