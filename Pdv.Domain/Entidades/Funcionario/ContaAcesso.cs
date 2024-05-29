363414a40434e639f400d5c68eafbba814958df0
Marcus Vinicius Nobre Carraschi <marcus.carraschi@linx.com.br>
2024-05-29 20:53:38 -0300

    🏗️ - Add some new structure of employe

---
+++ Pdv.Domain/Entidades/Funcionario/ContaAcesso.cs
@@ -0,0 +1,14 @@
+﻿using Pdv.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pdv.Domain.Entidades.Funcionario
+{
+    public abstract class ContaAcesso
+    {
+        public string Usuario {  get; set; }
+        public string Senha { get; set; }
+        public TipoAcesso TipoAcesso { get; set; } = TipoAcesso.Funcionario;
+    }
+}
